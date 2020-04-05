a5f4e41203f8ce077d85a08252d876a9b1fccb17
safayetjamil647 <work.jamil4@gmail.com>
2020-04-05 14:15:08 +0600

    first commit

---
+++ conditions.py
@@ -0,0 +1,12 @@
+is_hot = False
+is_cold= True
+if is_hot:
+    print("its a hot day")
+    print("drinh wteryy")
+elif is_cold:
+    print("its a cold day")
+    print("wear clodes")
+
+else:
+    print("whatever she lyk")
+print("its a lovely day")
